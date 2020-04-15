0b7be0a5ed90e0c654c53271098aada9a5559606
JavorovNetIt <k.javorov.net.it@gmail.com>
2020-04-15 12:40:33 +0300

    Added Demo project

---
+++ Shop/Shop/ShopForm.Designer.cs
@@ -0,0 +1,252 @@
+﻿namespace Shop
+{
+    partial class ShopForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ShopForm));
+            this.gvMenu = new System.Windows.Forms.DataGridView();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.gvShoppingCart = new System.Windows.Forms.DataGridView();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tbShoppingCartSum = new System.Windows.Forms.TextBox();
+            this.tbCashBox = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.gvMenu)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.panel4.SuspendLayout();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.gvShoppingCart)).BeginInit();
+            this.panel3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // gvMenu
+            // 
+            this.gvMenu.AllowUserToAddRows = false;
+            this.gvMenu.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.gvMenu.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.gvMenu.Location = new System.Drawing.Point(0, 57);
+            this.gvMenu.Name = "gvMenu";
+            this.gvMenu.RowHeadersWidth = 51;
+            this.gvMenu.RowTemplate.Height = 24;
+            this.gvMenu.Size = new System.Drawing.Size(833, 153);
+            this.gvMenu.TabIndex = 0;
+            this.gvMenu.DoubleClick += new System.EventHandler(this.dataGridView1_DoubleClick);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.gvMenu);
+            this.panel1.Controls.Add(this.panel4);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(833, 210);
+            this.panel1.TabIndex = 1;
+            // 
+            // panel4
+            // 
+            this.panel4.Controls.Add(this.label3);
+            this.panel4.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel4.Location = new System.Drawing.Point(0, 0);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(833, 57);
+            this.panel4.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(3, 8);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(125, 46);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Меню";
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.gvShoppingCart);
+            this.panel2.Controls.Add(this.panel3);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel2.Location = new System.Drawing.Point(0, 210);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(833, 181);
+            this.panel2.TabIndex = 2;
+            // 
+            // gvShoppingCart
+            // 
+            this.gvShoppingCart.AllowUserToAddRows = false;
+            this.gvShoppingCart.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.gvShoppingCart.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.gvShoppingCart.Location = new System.Drawing.Point(0, 57);
+            this.gvShoppingCart.Name = "gvShoppingCart";
+            this.gvShoppingCart.RowHeadersWidth = 51;
+            this.gvShoppingCart.RowTemplate.Height = 24;
+            this.gvShoppingCart.Size = new System.Drawing.Size(833, 124);
+            this.gvShoppingCart.TabIndex = 1;
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.button3);
+            this.panel3.Controls.Add(this.button2);
+            this.panel3.Controls.Add(this.button1);
+            this.panel3.Controls.Add(this.label2);
+            this.panel3.Controls.Add(this.label1);
+            this.panel3.Controls.Add(this.tbShoppingCartSum);
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel3.Location = new System.Drawing.Point(0, 0);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(833, 57);
+            this.panel3.TabIndex = 2;
+            // 
+            // button3
+            // 
+            this.button3.Image = ((System.Drawing.Image)(resources.GetObject("button3.Image")));
+            this.button3.Location = new System.Drawing.Point(759, 8);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(46, 43);
+            this.button3.TabIndex = 7;
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button2
+            // 
+            this.button2.Image = ((System.Drawing.Image)(resources.GetObject("button2.Image")));
+            this.button2.Location = new System.Drawing.Point(711, 8);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(42, 43);
+            this.button2.TabIndex = 6;
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Image = ((System.Drawing.Image)(resources.GetObject("button1.Image")));
+            this.button1.Location = new System.Drawing.Point(662, 8);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(43, 43);
+            this.button1.TabIndex = 5;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(3, 8);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(189, 46);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Кошница";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(425, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(116, 25);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Обща сума";
+            // 
+            // tbShoppingCartSum
+            // 
+            this.tbShoppingCartSum.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.tbShoppingCartSum.Location = new System.Drawing.Point(556, 19);
+            this.tbShoppingCartSum.Name = "tbShoppingCartSum";
+            this.tbShoppingCartSum.Size = new System.Drawing.Size(100, 30);
+            this.tbShoppingCartSum.TabIndex = 3;
+            // 
+            // tbCashBox
+            // 
+            this.tbCashBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.tbCashBox.Location = new System.Drawing.Point(662, 481);
+            this.tbCashBox.Name = "tbCashBox";
+            this.tbCashBox.Size = new System.Drawing.Size(100, 30);
+            this.tbCashBox.TabIndex = 4;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(551, 484);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(54, 25);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Каса";
+            // 
+            // ShopForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(833, 569);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.tbCashBox);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "ShopForm";
+            this.Text = "Магазин";
+            ((System.ComponentModel.ISupportInitialize)(this.gvMenu)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel4.ResumeLayout(false);
+            this.panel4.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.gvShoppingCart)).EndInit();
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView gvMenu;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.DataGridView gvShoppingCart;
+        private System.Windows.Forms.TextBox tbShoppingCartSum;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox tbCashBox;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ Shop/Shop/ShopForm.cs
@@ -0,0 +1,188 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Shop
+{
+    public partial class ShopForm : Form
+    {
+        public ShoppingCart shoppingCart = new ShoppingCart();
+
+        public BindingSource bindingSourceForShoppingCart = new BindingSource();
+        public BindingSource bindingSourceForMenu = new BindingSource();
+
+        decimal CashBox = 0.0m;
+        public ShopForm()
+        {
+            InitializeComponent();
+
+            UpdateUI();
+
+            SetGridColumns();
+
+            SetDataSource();
+
+            SetDataBindings();
+        }
+
+        #region  Methods
+        private void SetDataBindings()
+        {
+            ClearDataBindings(this);
+
+            tbShoppingCartSum.Text = string.Format("{0:C}", shoppingCart.SumOfAllProducts);
+            tbCashBox.Text = string.Format("{0:C}", CashBox);
+            //var binding = tbShoppingCartSum.DataBindings.Add("Text", shoppingCart, "SumOfAllProducts");
+            //binding.DataSourceUpdateMode = DataSourceUpdateMode.OnPropertyChanged;
+        }
+
+        private void ClearDataBindings(Control control)
+        {
+            foreach (Control ctrl in control.Controls)
+            {
+                ctrl.DataBindings.Clear();
+
+                ClearDataBindings(ctrl);
+            }
+        }
+
+        private void SetDataSource()
+        {
+            bindingSourceForMenu.DataSource = Product.GetProducts();
+            gvMenu.DataSource = bindingSourceForMenu;
+
+            bindingSourceForShoppingCart.DataSource = shoppingCart.Products;
+
+            gvShoppingCart.DataSource = bindingSourceForShoppingCart;
+        }
+
+        private void UpdateUI()
+        {
+            gvMenu.AutoGenerateColumns = false;
+            gvShoppingCart.AutoGenerateColumns = false;
+        }
+
+        private void SetGridColumns()
+        {
+
+            DataGridViewColumn col = new DataGridViewColumn();
+            col.CellTemplate = new DataGridViewTextBoxCell();
+            col.HeaderText = "Име";
+            col.DataPropertyName = "Name";
+            col.ReadOnly = true;
+            gvMenu.Columns.Add(col);
+
+            col = new DataGridViewColumn();
+            col.CellTemplate = new DataGridViewTextBoxCell();
+            col.HeaderText = "Цена";
+            col.DataPropertyName = "Price";
+            col.ReadOnly = true;
+            gvMenu.Columns.Add(col);
+
+            col = new DataGridViewColumn();
+            col.CellTemplate = new DataGridViewTextBoxCell();
+            col.HeaderText = "к-во";
+            col.DataPropertyName = "Qty";
+            col.ReadOnly = false;
+            gvMenu.Columns.Add(col);
+
+            col = new DataGridViewColumn();
+            col.CellTemplate = new DataGridViewTextBoxCell();
+            col.HeaderText = "Име";
+            col.DataPropertyName = "Name";
+            col.ReadOnly = true;
+            gvShoppingCart.Columns.Add(col);
+
+            col = new DataGridViewColumn();
+            col.CellTemplate = new DataGridViewTextBoxCell();
+            col.HeaderText = "Цена";
+            col.DataPropertyName = "Price";
+            col.ReadOnly = true;
+            gvShoppingCart.Columns.Add(col);
+
+            col = new DataGridViewColumn();
+            col.CellTemplate = new DataGridViewTextBoxCell();
+            col.HeaderText = "к-во";
+            col.DataPropertyName = "Qty";
+            col.ReadOnly = false;
+            gvShoppingCart.Columns.Add(col);
+
+            col = new DataGridViewColumn();
+            col.CellTemplate = new DataGridViewTextBoxCell();
+            col.HeaderText = "Обща цена";
+            col.DataPropertyName = "Sum";
+            col.ReadOnly = false;
+            gvShoppingCart.Columns.Add(col);
+        }
+
+        private void AddProductToShoppingCart(Product product)
+        {
+            Product productToAdd = new Product()
+            {
+                Name = product.Name,
+                Price = product.Price,
+                Qty = product.Qty
+            };
+
+            shoppingCart.Products.Add(productToAdd);
+            bindingSourceForShoppingCart.ResetBindings(false);
+
+            product.Qty = 0;
+            bindingSourceForMenu.ResetBindings(false);
+        }
+
+        private void ClearShoppingCart()
+        {
+            shoppingCart.Products.Clear();
+            bindingSourceForShoppingCart.ResetBindings(false);
+            tbShoppingCartSum.Text = "0";
+        }
+
+        #endregion
+
+        #region Events
+        private void dataGridView1_DoubleClick(object sender, EventArgs e)
+        {
+            var view = sender as DataGridView;
+
+            Product product = (Product)view.CurrentRow.DataBoundItem;
+
+            if (product.Qty > 0)
+            {
+                AddProductToShoppingCart(product);
+
+            }
+            else
+            {
+                MessageBox.Show("Моля въведете количество на желания продукт");
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SetDataBindings();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            ClearShoppingCart();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            CashBox += shoppingCart.SumOfAllProducts;
+
+            SetDataBindings();
+
+            ClearShoppingCart();
+        }
+
+        #endregion
+    }
+}
